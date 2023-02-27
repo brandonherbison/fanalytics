d50ea40322f2c006e050cd01cf83af771b9f9d66
Brandon Herbison <herbisonbrandon@gmail.com>
2023-02-27 11:07:10 -0500

    created application views and home route path

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,4 +1,6 @@
+import { Route, Routes } from "react-router-dom"
 import { Navbar } from "./Navbar"
+import { Home } from "./Home"
 
 
 
@@ -6,6 +8,9 @@ export const ApplicationViews = () => {
     return (
         <>
         <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
         </>
     )
 }
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+
+
+
+
+export const Home = () => {
+
+    
+
+    return (
+        <>  
+            <section>
+            <h1>Welcome to Fanalytics!</h1>
+            <p>An intuitive way for fans to analyze NFL data.</p>
+            </section>
+        </>
+    )
+}
